0fedb23b5b539729acd1873807be363b92bcbb6c
dariaprikhodko <favour313@mail.ru>
2018-11-28 19:25:23 +1100

    задание 2 учебного проекта

--- js/stat.js
+++ js/stat.js
@@ -11,20 +11,13 @@ var TEXT_WIDTH = 50;
 var BAR_HEIGHT = '150';
 var BAR_WIDTH = 40;
 
+
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getPlayerColor = function (name) {
-  if (name === 'Вы') {
-    return 'rgba(255, 0, 0, 1)';
-  } else {
-    return ['rgb(0, 0, ', (Math.random() * 255).toFixed(0), ')'].join('');
-  }
-};
-
-/* var getMaxElement = function (arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -34,7 +27,7 @@ var getPlayerColor = function (name) {
   }
 
   return maxElement;
-}; */
+};
 
 var renderTitle = function (ctx) {
   ctx.font = '16px PT Mono';
@@ -44,20 +37,33 @@ var renderTitle = function (ctx) {
   ctx.fillText('Список результатов:', CLOUD_X + GAP + FONT_GAP, CLOUD_Y + GAP + FONT_GAP * 2);
 };
 
-var renderCharts = function (ctx, names) {
+var renderCharts = function (ctx, names, times, maxTime) {
+  var step = BAR_HEIGHT / maxTime;
+
   for (var i = 0; i < names.length; i++) {
+    var columnHeight = step * times[i];
 
     ctx.fillText(names[i], CLOUD_X + TEXT_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - FONT_GAP);
-    ctx.fillRect(CLOUD_X + TEXT_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - BAR_HEIGHT - FONT_GAP, BAR_WIDTH, BAR_HEIGHT);
-    ctx.fillStyle = getPlayerColor(names[i]);
+
+    var columnY = ctx.canvas.clientHeight - columnHeight - 60;
+
+    ctx.fillRect(
+        CLOUD_X + TEXT_WIDTH + (BAR_WIDTH + BAR_GAP) * i,
+        columnY, // CLOUD_Y + CLOUD_HEIGHT - BAR_HEIGHT - FONT_GAP,
+        BAR_WIDTH,
+        columnHeight
+    );
+
+    ctx.fillStyle = (name[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255,' + Math.random() + ')';
   }
 };
 
-window.renderStatistics = function (ctx, names) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   renderTitle(ctx);
-  renderCharts(ctx, names);
 
-  /* var maxTime = getMaxElement(times);*/
+  var maxTime = getMaxElement(times);
+
+  renderCharts(ctx, names, times, maxTime);
 };
